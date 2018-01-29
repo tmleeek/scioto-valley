665ec7758b697404664a286c01aeca333d9653a1
brads07 <4BradyKins>
2018-01-29 11:18:44 -0500

    Update Google Remarketing

--- app/code/local/Watsons/TrackingPixels/Block/Pixel.php
+++ app/code/local/Watsons/TrackingPixels/Block/Pixel.php
@@ -57,21 +57,19 @@ class Watsons_TrackingPixels_Block_Pixel extends Mage_Core_Block_Template
             $mathTag ='';
         }
         $googleRemarketing = '
-                <!-- Google Code for Remarketing Tag -->
+                <!-- Google Remarketing Tag For Scioto Valley -->
                 <script type="text/javascript">
                 /* <![CDATA[ */
-                var google_conversion_id = 982616289;
-                var google_custom_params = window.google_tag_params; var google_remarketing_only
-                = true;
+                var google_conversion_id = 823553192;
+                var google_custom_params = window.google_tag_params;
+                var google_remarketing_only = true;
                 /* ]]> */
                 </script>
                 <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
                 </script>
                 <noscript>
                 <div style="display:inline;">
-                <img height="1" width="1" style="border-style:none;" alt=""
-                src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/982616289/?guid=O
-                N&amp;s cript=0"/>
+                <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/823553192/?guid=ON&amp;script=0"/>
                 </div>
                 </noscript>';
         $segmentPixel = '
